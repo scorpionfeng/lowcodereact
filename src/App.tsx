b8d4d8ed8d11b8a876e05529796b9dee3a872889
scorpionfeng <dcfn4@163.com>
2021-09-26 20:27:15 +0800

    test useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const fullname=useMemo(()=>{
+    return  `my name ${username} ${password}`
+  },[username])
+
   return (
     <div className="App">
-      lowcode
+      <h1>hello,world</h1>
+      <input type='text' value={username} onChange={e=>setUsername(e.target.value)} />
+      <input type='text' value={password} onChange={e=>setPassword(e.target.value)}/>
+      {JSON.stringify({username,password,fullname})}
     </div>
   );
 }
